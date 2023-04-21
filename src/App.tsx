bbdc7c766dadfd1d011271e2e6400fe182789cc8
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-04-21 14:54:50 +0100

    hero section edited

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import './App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import Navbar from './Shared/Navbar'
+import Hero from './Home/Hero'
 
 function App() {
   return (
     <div className="Appx">
 {/* <h1 className='text-3xl font-bold text-blue-600' >  my name is blade heda </h1> */}
 <Navbar/>
+<Hero/>
     </div>
   )
 }
