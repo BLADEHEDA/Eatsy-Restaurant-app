a39367c54b07d61d1eeebfb710ba8c3a4cab6e36
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-04-24 12:55:30 +0100

    added background image in cart

--- src/Components/Home/Seller.tsx
+++ src/Components/Home/Seller.tsx
@@ -11,10 +11,11 @@ import burger from "/assets/image_product_01.jpg"
 
  
 // the menu button  
-const MenuBtn=()=>{
+const MenuBtn=(props)=>{
     return(
-        <main className="btn">
-            <button className="menu-btn rounded-[50%] py-[1.5em] px-[1.75em] border-2   border-[#e84242]  ">
+        <main className="btn"  >
+            <button style={props.style} 
+            className="menu-btn rounded-[50%] py-[1.5em] px-[1.75em] border-2   border-[#e84242]  ">
             <div className="left1"> <FontAwesomeIcon className='text-[1em] text-[#e84242] ' icon={faPlus} /> </div>
        
             </button>
@@ -51,30 +52,35 @@ const Menu=(props)=>{
 // building the component of the cards for the menu section 
 const Menu1=(props)=>{
     return(
-        <main className="menu pb-[1.5em] ">
-            <section className="maini-menu-div bg-[#FEFEFE] rounded-[20px] mx-[1.5em] p-[1.5em] ">
-                <p className="menu-type font-[400] text-[black] "> {props.type} </p>
-                <h1 className="menu-name text-[2em] font-[800] text-[#121212] ">{props.name} </h1>
-                <h1 className="menu-nam text-[2em] font-[800] text-[#121212]">{props.name1} </h1>
-                <div className="menu-img-div">
-                    <img src={props.src} alt={props.name} className="menu-img object-cover " />
-                </div>
-                <section className="menu-bottom flex justify-between ">   
+        <main className="menu pb-[1.5em] "  >
+            {/* pass the background image a props  */}
+            <section className="maini-menu-div rounded-[20px] mx-[1.5em] p-[1.5em]  "
+                style={  { 
+                    backgroundImage: `url(${props.backgroundImage})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat"
+            }}
+                  
+                  > 
+                <p className="menu-type font-[400] text-[white] "> {props.type} </p>
+                <h1 className="menu-name text-[2em] font-[800] text-[white]   ">{props.name} </h1>
+                <h1 className="menu-nam text-[2em] font-[800] text-[white] ">{props.name1} </h1>
+                <section className="menu-bottom flex justify-between pt-[20em] ">   
              
-                <article className="menu-left">
+                <article className="menu-left text-[white]  ">
                 <p className="menu-price text-[2em] font-[800]"> {props.price} </p>
                 <p className="menu-specs text-[15px] mt-[-0.15em] "> {props.specs} </p>
                 </article>
 
                 <article className="menu-right">
-                    <MenuBtn/>
+                    <MenuBtn  style={{background:"transparent"} } />
                 </article>
                 </section>
             </section>
         </main>
     )
 }
-
 
 const Seller = () => {
   return (
@@ -86,7 +92,10 @@ const Seller = () => {
         <Menu type="MEALS" name="BaconBacon" src={burger3} name1="Cheeseburger" price="$8.00" specs="220gr / 600 cal" />
         <Menu type="CHICKEN" name="CrazyBeefTuna" src={burger4} name1="Burger" price="$13.00" specs="220gr / 600 cal" />
         <Menu type="VEGAN" name="ShroomBacon" src={burger5} name1="Burger" price="$10.00" specs="220gr / 600 cal" />
-        <Menu1 type="VEGAN" name="ShroomBacon" src={burger5} name1="Burger" price="$10.00" specs="220gr / 600 cal" />
+        <Menu1 backgroundImage={burger} type="VEGAN" name="Bøfsandwich"  price="$8.00" specs="220gr / 600 cal" />
+      
+      
+       
     </div>
       
     </main>
