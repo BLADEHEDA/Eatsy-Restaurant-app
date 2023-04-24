0c514ee9d1227ba2edc358b3212298a414fad29d
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-04-24 13:35:09 +0100

    changes to the main div section

--- src/Components/Home/Seller.tsx
+++ src/Components/Home/Seller.tsx
@@ -7,9 +7,9 @@ import burger2 from "/assets/product_02-640x640.jpg"
 import burger3 from "/assets/product_04-640x640.jpg"
 import burger4 from "/assets/product_09-640x640.jpg"
 import burger5 from "/assets/product_08-640x640.jpg" 
-import burger from "/assets/image_product_01.jpg"
-
- 
+import burger from "/assets/image_product_01.jpg"    
+import burgerA from "/assets/image_product_02.jpg"  
+import burgerB from "/assets/image_product_03.jpg"
 // the menu button  
 const MenuBtn=(props)=>{
     return(
@@ -66,7 +66,7 @@ const Menu1=(props)=>{
                 <p className="menu-type font-[400] text-[white] "> {props.type} </p>
                 <h1 className="menu-name text-[2em] font-[800] text-[white]   ">{props.name} </h1>
                 <h1 className="menu-nam text-[2em] font-[800] text-[white] ">{props.name1} </h1>
-                <section className="menu-bottom flex justify-between pt-[20em] ">   
+                <section className="menu-bottom flex justify-between" style={props.style} >   
              
                 <article className="menu-left text-[white]  ">
                 <p className="menu-price text-[2em] font-[800]"> {props.price} </p>
@@ -81,22 +81,63 @@ const Menu1=(props)=>{
         </main>
     )
 }
+// building the component of the cards for the menu section 
+const Menu2=(props)=>{
+    return(
+        <main className="menu pb-[1.5em] "  >
+            {/* pass the background image a props  */}
+            <section className="maini-menu-div rounded-[20px] mx-[1.5em] p-[1.5em]  "
+                style={  { 
+                    backgroundImage: `url(${props.backgroundImage})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat"
+            }}
+                  
+                  > 
+                <p className="menu-type font-[300] text-[white] "> {props.type} </p>
+                <h1 className="menu-name text-[1.5em] font-[800] text-[white]   ">{props.name} </h1>
+                <h1 className="menu-nam text-[2em] font-[800] text-[white] ">{props.name1} </h1>
+                <section className="menu-bottom flex justify-between" style={props.style} >   
+             
+                <article className="menu-left text-[white]  ">
+                <p className="menu-price text-[1.5em] font-[800]"> {props.price} </p>
+                <p className="menu-specs text-[15px] mt-[-0.15em] "> {props.specs} </p>
+                </article>
+
+                <article className="menu-right">
+                    <MenuBtn  style={{background:"transparent"} } />
+                </article>
+                </section>
+            </section>
+        </main>
+    )
+}
+
 
 const Seller = () => {
   return (
     <main className=' Seller Hero-main pb-[10em] '>
     <h1 className="sellers text-[#F4E6C0]  font-['Rakkas'] text-center  pb-[1em] ">Bestsellers</h1>
-    <div className="menus-seller">
+    <section className="menus-seller">
+        <div className="seller-div-1">
         <Menu type="BEEF" name="Bacon+Cheese" src={burger1} name1="+Green Burger" price="$6.00" specs="220gr / 600 cal" />
-        <Menu type="BEEF" name="Burger" src={burger2} name1="+Green Burger" price="$12.00" specs="220gr / 600 cal" />
+        <Menu type="BEEF" name="Black Angus" src={burger2} name1="Burger" price="$12.00" specs="220gr / 600 cal" />
+        <Menu1 style={{paddingTop:'20em'}} 
+        backgroundImage={burger} type="VEGAN" name="Bøfsandwich"  price="$8.00" specs="220gr / 600 cal" />
+        </div>
+      <Menu1   style={{paddingTop:'7em'}} backgroundImage={burgerA} 
+                 price="$11.00" specs="220gr / 600 cal" />
         <Menu type="MEALS" name="BaconBacon" src={burger3} name1="Cheeseburger" price="$8.00" specs="220gr / 600 cal" />
         <Menu type="CHICKEN" name="CrazyBeefTuna" src={burger4} name1="Burger" price="$13.00" specs="220gr / 600 cal" />
         <Menu type="VEGAN" name="ShroomBacon" src={burger5} name1="Burger" price="$10.00" specs="220gr / 600 cal" />
-        <Menu1 backgroundImage={burger} type="VEGAN" name="Bøfsandwich"  price="$8.00" specs="220gr / 600 cal" />
       
+        <Menu2  style={{paddingTop:'1.5em'}}  backgroundImage={burgerB} type="MEALS"
+         name="#stayhome kit: 6 mini burger + 6 Pack Kraft beer"  price="$22.00" specs="220gr / 600 cal" />
+     
       
        
-    </div>
+    </section>
       
     </main>
   )
