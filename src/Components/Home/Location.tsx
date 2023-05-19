6939f868e8e7556dd521b62a406358102e9bf769
BLADE-HEDA <nkwetakembrunodudu9@gmail.com>
2023-05-19 08:22:40 +0100

    new changes done

--- src/Components/Home/Location.tsx
+++ src/Components/Home/Location.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
+import "./Home.css"
 
 const Location = () => {
   return (
-    <main className='bg-[#F5E7C1] mt-[-1px] pb-[20em] ' >
-    <h1>Contactless deliverable  available</h1>
-    <p>
-        
-    </p>
+    <main className='bg-[#F5E7C1] mt-[-1px] pb-[20em] px-[1.5em] pt-[5em] ' >
+    <h1 className="text-[2.3em] text-[#E84242] font-['Rakkas'] font-[700]
+     " >Contactless deliverable  available</h1>
     </main>
 
   )
