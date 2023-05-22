f6e2a94c4ab123bfaf23916af6e56452257132ee
BLADE-HEDA <nkwetakembrunodudu9@gmail.com>
2023-05-22 10:34:22 +0100

    text adjustments maded

--- src/Components/Home/Location.tsx
+++ src/Components/Home/Location.tsx
@@ -16,24 +16,24 @@ const Location = () => {
      <section className="locations">
       <div  className="locate mb-[1em] ">
         <h2 className="locate-head font-[600] text-[1.3em] text-[black] mb-[0.75em]">Location</h2>
-        <p className="locate-content">60 East 65th Street, New York</p>
+        <p className="locate-content text-[#201912]">60 East 65th Street, New York</p>
       </div>
 
       <div  className="locate mb-[1em] ">
         <h2 className="locate-head font-[600] text-[1.3em] text-[black] mb-[0.75em] ">Delivery hours</h2>
         <h3 className="locate-head font-[500] text-[1em] text-[black]">Monday – Saturday:</h3>
-        <p className="locate-content">11AM – 11PM</p>
+        <p className="locate-content text-[#201912]">11AM – 11PM</p>
         <h3 className="locate-head font-[500] text-[1em] text-[black]  mt-[0.75em] ">Sunday:</h3>
-        <p className="locate-content">111AM – 08PM</p>
+        <p className="locate-content text-[#201912]">111AM – 08PM</p>
       </div>
 
       <div  className="locate mb-[1em] ">
         <h2 className="locate-head font-[600] text-[1.3em] text-[black] mb-[0.75em] ">Takeaway hours</h2>
         <h3 className="locate-head font-[500] text-[1em] text-[black]">Monday – Saturday:</h3>
-        <p className="locate-content">01PM – 10PM</p>
+        <p className="locate-content text-[#201912]">01PM – 10PM</p>
         <div className="flex mt-[0.75em]"> 
         <p className="locate-head font-[500] text-[1em] text-[black]">Sunday: </p>
-        <p className="locate-content ml-1 ">Closed</p>
+        <p className="locate-content ml-1 text-[#201912] ">Closed</p>
         </div>
       </div>
      </section>   
