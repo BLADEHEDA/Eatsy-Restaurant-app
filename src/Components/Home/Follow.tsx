79ba18dac34134c5e574121e6270a84d2ca2f884
BLADE-HEDA <nkwetakembrunodudu9@gmail.com>
2023-05-22 19:16:36 +0100

    changes done to footer

--- src/Components/Home/Follow.tsx
+++ src/Components/Home/Follow.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import hero from "../../../public/assets/hero_home_02.jpg"
+import hero from "/assets/hero_home_02.jpg"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faPlus } from '@fortawesome/free-solid-svg-icons'
 import {faFacebook, faInstagram , faTwitter} from '@fortawesome/free-brands-svg-icons'
 
 const Follow = () => {
   return (
-    <main className='mb-[0em]' > 
+    <main className='mb-[0em]  ' > 
     <div className='follow mt-[-3.99em] pb-[10em] flex justify-center 
-    items-center w-full pt-[7em]  ' 
+    items-center w-full pt-[7em] mb-[-1px]  ' 
     style={  { 
       backgroundImage: `url(${hero})`,
         backgroundSize: "cover",
@@ -36,14 +36,14 @@ const Follow = () => {
     </div>
     <svg 
         style={  { 
-        // backgroundImage: `url(../../../public/assets/hero_home_02.jpg)`,
         backgroundImage: `url(${hero})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat"
 }}
-    className='mt-[-1px] bg-[#121212] ' xmlns="http://www.w3.org/2000/svg" 
-    viewBox="0 0 1440 320"><path fill="#F5E7C1" 
+    className='mt-[-1px] bg-[#121212]  ' xmlns="http://www.w3.org/2000/svg" 
+    viewBox="0 0 1440 320">
+    <path fill="#F5E7C1" 
     fill-opacity="1" d="M0,160L60,176C120,192,240,224,360,208C480,192,600,128,720,117.3C840,107,960,149,1080,170.7C1200,192,1320,192,1380,192L1440,192L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
     </svg>
     </main>
--- src/Components/Shared/Footer.tsx
+++ src/Components/Shared/Footer.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
-
+import React from 'react';
+import herofooter from "/assets/hero_footer.jpg"
+import logo from "/assets/footer_logo.png"
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <main className='pb-[10em] '>
+<svg 
+ style={  { 
+  backgroundImage: `url(${herofooter})`,
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  zIndex: 100
+}}
+className='bg-[red] mt-[-0.3px] '
+xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#F5E7C1" fill-opacity="1" d="M0,256L48,218.7C96,181,192,107,288,117.3C384,128,480,224,576,245.3C672,267,768,213,864,197.3C960,181,1056,203,1152,181.3C1248,160,1344,96,1392,64L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+</svg>
+      <div className="container mt-[-1px] pt-[5em] pb-[10em]  "
+       style={  { 
+        backgroundImage: `url(${herofooter})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+      >
+        <section className='text-center' > 
+        <div className='img flex justify-center mb-[1em]  ' >
+      <img src={ logo} className='w-[8em] ' alt="Wavy image" />
+      </div>
+      <p className='text-[white] font-[500] text-[1.55em] w-[80%] my-0 mx-auto leading-[1.2em] ' >
+        Stay informed about special offers </p>
+        <p className='text-[white] font-[400] text-[1.1em] w-[80%] my-2 mx-auto leading-[1.2em] ' >
+        Subscribe to our bews letter</p>
+      <div className="inputdiv">
+        <input type="email" placeholder='Enter Your Email' 
+        className="input p-3 rounded-[1.2em] mt-[1em] " />
+      </div>
+      </section>
+    </div>
+    </main>
+  );
+};
 
-export default Footer
+export default Footer;
